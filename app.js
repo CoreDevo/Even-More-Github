ab63715a0ca768a13788d924a1cf0d0806d6fa81
ckyue <yuechuankai@gmail.com>
2016-06-05 04:15:04 -0400

    scraped user language preference

---
+++ app.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var app = express();
+var https = require("https");
+var sortJson = require('sort-json');
+var userName='ckyue';
+var options = {
+host :"api.github.com",
+path : '/users/'+userName+'/repos',
+method : 'GET',
+headers: {'User-Agent':'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'}
+}
+
+var request = https.request(options, function(response){
+var body = '';
+response.on('data',function(chunk){
+    body+=chunk;
+});
+response.on('end',function(){
+    var json = JSON.parse(body);
+    var languages = [];
+    json.forEach(function(repo){
+        languages.push(repo.language);
+    });
+    // console.log(languages);
+    calculateWeight(languages);
+});
+
+});
+request.on('error', function(e) {
+    console.error('and the error is '+e);
+});
+request.end();
+
+function calculateWeight(arrayElements){
+    var counts = {};
+    // console.log(arrayElements)
+    arrayElements.forEach(function(x) {
+        counts[x] = (counts[x] || 0)+1;
+    });
+    // console.log(counts);
+    var totalLanguages = getSum(counts);
+    // console.log(totalLanguages);
+    for(var language in counts){
+        // console.log(counts[language])
+        counts[language] = counts[language]/totalLanguages;
+    }
+    counts = sortProperties(counts);
+    counts.reverse();
+    var sortedList = [];
+    for(var element in counts){
+      counts[element] = JSON.stringify(counts[element]).replace(/,/g , ": ");
+      counts[element] = counts[element].replace(/\[/g, "{");
+      counts[element] = counts[element].replace(/\]/g, "}")
+      sortedList.push(counts[element]);
+    }
+    sortedList = JSON.stringify(sortedList).replace(/["]+/g, '').replace(/\\/g, "'")
+    console.log(sortedList);
+
+    // var objKeysRegex = /({|,)(?:\s*)(?:')?([A-Za-z_$\.][A-Za-z0-9_ \-\.$]*)(?:')?(?:\s*):/g;// look for object names
+    // var counts = JSON.stringify(counts).replace(/(\s*?{\s*?|\s*?,\s*?)(['"])?([a-zA-Z0-9]+)(['"])?:/g, '$1"$3":');
+    // console.log(counts)
+
+}
+
+function getSum(obj){
+  var sum = 0;
+    for(var key in obj){
+        sum = sum + Number(obj[key]);
+      }
+    return sum
+}
+
+function sortProperties(obj)
+{
+    var sortable=[];
+    for(var key in obj){
+        if(obj.hasOwnProperty(key)){
+            sortable.push([key, obj[key]]);
+          }
+    }
+    // console.log(sortable)
+    sortable.sort(function(a, b)
+    {
+        var x=a[1],
+            y=b[1];
+        return x<y ? -1 : x>y ? 1 : 0;
+    });
+    return sortable;
+}
