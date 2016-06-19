a905e80814d7aef46a42b13e513f9e85ba9823db
ckyue <yuechuankai@gmail.com>
2016-06-18 21:53:47 -0400

    github oauth done, format csv data for each user done

--- app.js
+++ app.js
@@ -9,22 +9,23 @@ var csv = require('ya-csv');
 var csvWriter = csv.createCsvFileWriter('dataset.csv');
 var dataArray = [];
 var input;
-var combinedList = [];
+var combinedList = [];//NOTE:needs to be free
+var starredRepoURLs = [];//NOTE:needs to be free
 var starredRepoNumberCounter = 0;
 
 app.get('/', function (req, res) {
     res.send(input)
 });
-//
-//get more users
-GetUserStarredRepo("ckyue");//DEBUGGING
-GetUserOwnRepo("ckyue");//DEBUGGING
-function GetUserStarredRepo(username){
+
+var GithubOAuth = function(){
     var options = {
       host :"api.github.com",
-      path : '/users/'+username+'/starred',
-      method : 'GET',
-      headers: {'User-Agent':'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'}
+      path : '/',
+      method : 'POST',
+      headers: {
+        'User-Agent':'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+        'Authorization':'token '+'417b00012e761b62fb8a99d4ef12f70f6370118a'
+      }
     }
 
     var request = https.request(options, function(response){
@@ -35,19 +36,46 @@ function GetUserStarredRepo(username){
       response.on('end',function(){
         var json = JSON.parse(body);
         // console.log(json)
-        var starredRepoURLs = [];
+        });
+      });
+      request.on('error', function(e) {
+        console.error('and the error is '+e);
+      });
+      request.end();
+}();
+
+//get more users
+GetUserStarredRepo("ckyue");//DEBUGGING
+function GetUserStarredRepo(username){
+    var options = {
+      host :"api.github.com",
+      path : '/users/'+username+'/starred',
+      method : 'GET',
+      headers: {
+        'User-Agent':'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'
+      }
+    }
+
+    var request = https.request(options, function(response){
+      var body = '';
+      response.on('data',function(chunk){
+        body+=chunk;
+      });
+      response.on('end',function(){
+        var json = JSON.parse(body);
         json.forEach(function(repo){
-          //TODO:push link properly
           starredRepoURLs.push(repo.html_url);
         });
-          console.log(starredRepoURLs);
-          starredRepoNumberCounter = starredRepoURLs.length;//NOTE: maxx of 30 repos returned by github api
-          console.log(starredRepoNumberCounter)
+          // console.log(starredRepoURLs);
+          starredRepoNumberCounter = starredRepoURLs.length;//NOTE: max of 30 repos returned by github api
+          // console.log(starredRepoNumberCounter)
+          GetUserOwnRepo("ckyue");//DEBUGGING
       });
     });
     request.on('error', function(e) {
       console.error('and the error is '+e);
     });
+    console.log("get starred request end")
     request.end();
 }
 
@@ -100,14 +128,28 @@ function calculateWeight(arrayElements){
     sortedList = JSON.stringify(sortedList).replace(/["]+/g, '').replace(/\\/g, "'").replace(/'/g, '"');
     sortedList = JSON.parse(sortedList);
     combinedList = combineJsonObj(sortedList)
-    console.log(combinedList);
+    // console.log(combinedList);
     addToDataArray(combinedList);
     // exportToCSV(combinedList);
 }
 
 function addToDataArray(list){
   //TODO:format data array by number of user starred repo
-    dataArray.push(addToDataArray);
+    // console.log(starredRepoURLs)
+    list["url"] = ""
+    var dataArrayStrings = [];
+    starredRepoURLs.forEach(function(repoLink){
+      // console.log(repoLink)
+      list["url"] = repoLink;
+      // console.log(list)
+      dataArrayStrings.push(JSON.stringify(list));//NOTE: having issue appending object into Array
+    });
+    //inflate strings into JSON again due to that issue LOL
+    dataArrayStrings.forEach(function(string){
+      // console.log(string)
+      dataArray.push(JSON.parse(string))
+    })
+    console.log(dataArray)
 }
 function combineJsonObj(source) {
     var result = {};
@@ -147,9 +189,9 @@ function sortProperties(obj)
 }
 
 //last step after appending all data into dataArray
-function exportToCSV(combinedList){
+function exportToCSV(list){
   //combinedList is an array of JSON objects
-    json2csv({ data: combinedList, fields: fields }, function(err, csv) {
+    json2csv({ data: list, fields: fields }, function(err, csv) {
         if (err) console.log(err);
           console.log(csv);
           fs.writeFile('dataset.csv', csv, function(err) {
