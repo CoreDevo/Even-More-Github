6380e5535447cd6a78f00710a2abc7b62e55eab6
Chuankai Yue <yuechuankai@gmail.com>
2016-09-23 02:11:55 -0400

    scraped and analyzed

--- scraper.js
+++ scraper.js
@@ -1,20 +1,56 @@
 var gs = require('github-scraper');
-var urlArray = ['/CoreDevo/Drop-The-Beat',
-                '/CoreDevo/ExpresSJ',
-                '/CoreDevo/FGiP',
-                '/CoreDevo/LeapRevo'
-                ]
+var cheerio = require('cheerio')
+var request = require('request');
 var repoCounter = 0;
+var urlArray = new Array();
+var pageCounter = 0;
+
+//top 1000 Repos for now
+function scrapeRepoLink(pageCounter){
+    pageCounter++;
+    request({
+        method: 'GET',
+        url: 'https://github-ranking.com/repositories?page=' + pageCounter
+    },
+    function(err, response, body) {
+        if (err){
+            return console.error(err);
+        }
+
+        $ = cheerio.load(body);
+        $('a.list-group-item').each(function() {
+                var repoLink = $(this)[0].attribs.href
+                urlArray.push(repoLink)
+        });
+
+        if(pageCounter != 10){
+            console.log("retrieve Repo URLs from page " + pageCounter + " done!")
+            scrapeRepoLink(pageCounter)
+        }else{
+            console.log("starting to scrape " + urlArray.length + " repos :)")
+            recursiveGetRepoLangs("/CoreDevo/Even-More-Github")
+        }
+    });
+}
+
+
 
 function recursiveGetRepoLangs(url){
     repoCounter++;
     gs(url, function(err, data) {
-        console.log(data.langs); // or what ever you want to do with the data
+        if(data != null){
+            console.log(data.langs); //do whatever with data.langs then
+        }
+
         if(repoCounter != urlArray.length){
             recursiveGetRepoLangs(urlArray[repoCounter])
+        }else{
+            console.log("done")
         }
     })
 
 }
 
-recursiveGetRepoLangs("/CoreDevo/Even-More-Github")
+var init = function(){
+    scrapeRepoLink(pageCounter);
+}();
