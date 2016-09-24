ce61aeb864c6dcb06eff4b740d84455a24ce0e13
Chuankai Yue <yuechuankai@gmail.com>
2016-09-24 13:05:35 -0400

    more repo

--- scraper.js
+++ scraper.js
@@ -2,15 +2,17 @@ var gs = require('github-scraper');
 var cheerio = require('cheerio')
 var request = require('request');
 var repoCounter = 0;
-var urlArray = new Array();
+var userArray = new Array()
+var repoUrlArray = new Array();
 var pageCounter = 0;
+var userCounter = 0;
 
 //top 1000 Repos for now
-function scrapeRepoLink(pageCounter){
+function scrapeUserLink(pageCounter){
     pageCounter++;
     request({
         method: 'GET',
-        url: 'https://github-ranking.com/repositories?page=' + pageCounter
+        url: 'https://github-ranking.com/users?page=' + pageCounter
     },
     function(err, response, body) {
         if (err){
@@ -19,20 +21,47 @@ function scrapeRepoLink(pageCounter){
 
         $ = cheerio.load(body);
         $('a.list-group-item').each(function() {
-                var repoLink = $(this)[0].attribs.href
-                urlArray.push(repoLink)
+                var userLink = $(this)[0].attribs.href;
+                // userLink.replace('/', "");
+                userArray.push(userLink)
+                // console.log(userLink)
         });
 
         if(pageCounter != 10){
-            console.log("retrieve Repo URLs from page " + pageCounter + " done!")
+            console.log("retrieve User URLs from page " + pageCounter + " done!")
             console.log("==========================================================")
-            scrapeRepoLink(pageCounter)
+            scrapeUserLink(pageCounter)
         }else{
-            console.log("starting to scrape " + urlArray.length + " repos :)")
+            console.log("starting to scrape " + userArray.length + " repo links :)")
+            console.log("==========================================================")
+            // recursiveGetRepoLangs("/CoreDevo/Even-More-Github")
+            recursiveGetRepoLinks("stars/ckyue")
+        }
+    });
+}
+
+function recursiveGetRepoLinks(user){
+    userCounter++;
+    gs(user, function(err, data) {
+        if(data != null){
+            data.entries.forEach(function(entry){
+                var repoLink = entry;
+                repoUrlArray.push(repoLink)
+                // console.log(repoLink)
+            });
+        }
+
+        if(userCounter != userArray.length){
+            console.log("User scrapped: "+userCounter)
+            console.log("get "+repoUrlArray.length+" repository links")
+            recursiveGetRepoLinks("stars"+userArray[userCounter-1])
+        }else{
+            console.log("starting to scrape " + repoUrlArray.length + " repo Languages :)")
             console.log("==========================================================")
             recursiveGetRepoLangs("/CoreDevo/Even-More-Github")
         }
-    });
+    })
+
 }
 
 function recursiveGetRepoLangs(url){
@@ -42,8 +71,8 @@ function recursiveGetRepoLangs(url){
             console.log(data.langs); //do whatever with data.langs then
         }
 
-        if(repoCounter != urlArray.length){
-            recursiveGetRepoLangs(urlArray[repoCounter])
+        if(repoCounter != repoUrlArray.length){
+            recursiveGetRepoLangs(repoUrlArray[repoCounter-1])
         }else{
             console.log("===========================DONE===========================")
         }
@@ -53,5 +82,5 @@ function recursiveGetRepoLangs(url){
 
 var init = function(){
     console.log("===========================INIT===========================")
-    scrapeRepoLink(pageCounter);
+    scrapeUserLink(pageCounter);
 }();
